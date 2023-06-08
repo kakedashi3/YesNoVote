7f899b2737aa8c86b3a3a9827eda3da089d32dbb
kakedashi3 <riki_funk_x@yahoo.co.jp>
2023-06-08 12:54:36 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,67 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, Web3Button, useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const address = useAddress();
+  const contractAddress = "0x26275251a2380bCfa4F32059FbF83f31824FED4e"
+
+  const { contract } = useContract(contractAddress)
+  const { data: proposal, isLoading: proposalLoading } = useContractRead(contract, "proposals", [0]);
+  const { data: voter, isLoading: voterLoading } = useContractRead(contract, "voters", [0, address]);
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
-
-        <div className={styles.connect}>
+        <div className={styles.card}>
           <ConnectWallet />
-        </div>
+          <h1>Voting Dapp</h1>
+          <div>
+            {address ? (
+              <div>
+                {proposalLoading ? (
+                  <div>
+                    <p>ちょっとまってね...</p>
+                  </div>
+                ) : (
+                  <div>
+                    <h2>{proposal[0]}</h2>
+                    <div>
+                      <Web3Button
+                        contractAddress={contractAddress}
+                        action={(contract) => contract.call("Vote", [0, true])}
+                        isDisabled={voter}
+                      >Yes</Web3Button>
+                      
+                      <Web3Button
+                        contractAddress={contractAddress}
+                        action={(contract) => contract.call("Vote", [0, false])}
+                        isDisabled={voter}
+                      >No</Web3Button>
+                    </div>
+                    <div>
+                      {!voterLoading && voter && voter.voted ? (
+                        <div>
+                          <h3>Results:</h3>
+                          <p>Yes: {proposal[1].toNumber()}</p>
+                          <p>No: {proposal[2].toNumber()}</p>
+                        </div>
+                      ) : (
+                        <div>
+                          <p>You have not voted yet. Results will show after you vote.</p>
+                        </div>
+                      )}
+                    </div>
 
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <div>
+                <p>Connect your wallet to get started.</p>
+              </div>
+            )}
+          </div>
         </div>
       </main>
     </div>
